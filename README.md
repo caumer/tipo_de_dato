f7a225fb1ab9b90805fc78aac33a2af817143fef
César A. Medina R <csr.mdn@gmail.com>
2019-12-26 08:58:19 -0500

    tipo_de_dato

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# tipo_de_dato
- 
+<p align="center">
+  <img src="https://serving.photos.photobox.com/74658710c776094674480f20fd3e87b31aaf0bde8fb5b69bc0e49a6f954667fb76e301a8.jpg"> 
+</p>  
+
+About the project
+-------------
+Programa que determina que tipo de caracter es dato ingresado.
+
+About C#
+-------------
+C# is an elegant and type-safe object-oriented language that enables developers to build a variety of secure and robust applications that run on the .NET Framework. You can use C# to create Windows client applications, XML Web services, distributed components, client-server applications, database applications, and much, much more. Visual C# provides an advanced code editor, convenient user interface designers, integrated debugger, and many other tools to make it easier to develop applications based on the C# language and the .NET Framework.
+
+Technologies
+-------------
+- C#
+
+-------------
+<p align="center">
+- ACADEMIC PROJECT -
+</p>
+
+
---
+++ identificacion_caracteres/identificacion_caracteres/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace identificacion_caracteres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el caracter a evaluar: ");
+            char dato = char.Parse(Console.ReadLine());
+            //char dato2 = dato;
+            //string dato3 = dato;
+
+            if (dato >= 'a' && dato <= 'z')
+            {
+                Console.WriteLine("El caracter es letra minuscula");
+            }
+            else
+            {
+                //
+                if (dato >= 'A' && dato <= 'Z')
+                {
+                    Console.WriteLine("El caracter es letra mayuscula");
+                }
+                else
+                {
+                    if (dato >= '0' && dato <= '9')
+                    {
+                        Console.WriteLine("El caracter es numerico");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Es caracter");
+                    }
+                    
+                }
+            }
+           
+        }
+    }
+}
